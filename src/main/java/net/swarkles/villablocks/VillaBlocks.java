9c9ef73c4ccacd61a66b4827dd856841f0dad587
swarkles <agdaudelin@gmail.com>
2022-08-12 11:42:39 -0400

    testing commit

--- src/main/java/net/swarkles/villablocks/VillaBlocks.java
+++ src/main/java/net/swarkles/villablocks/VillaBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// VERY IMPORTANT COMMENT TESTING HERE
 public class VillaBlocks implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
